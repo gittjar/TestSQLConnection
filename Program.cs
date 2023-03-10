0e045ff53c640d47f5fc666b4b76e0059c432aaf
gittjar <112698736+gittjar@users.noreply.github.com>
2023-03-10 14:20:44 +0200

    Create Program.cs
    Test SQL connection from console. Dotnet run.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+using System;
+using Microsoft.Data.SqlClient;
+using System.Text;
+
+//
+// ADD THIS PACKAGE!
+// dotnet add package Microsoft.Data.SqlClient --version 5.1.0
+//
+//
+
+namespace sqltest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+            { 
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "yourservenamehere.database.windows.net"; 
+                builder.UserID = "yourusername";            
+                builder.Password = "yourpassword";     
+                builder.InitialCatalog = "yourdatabasename";
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    Console.WriteLine("\nQuery data example:");
+                    Console.WriteLine("=========================================\n");
+
+                    String sql = "SELECT name, collation_name FROM sys.databases";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                            }
+                        }
+                    }                    
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
